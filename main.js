906c107e2a8bdf7e96cdd44d55b04261e9b37eb0
JonCass <JCass1980@gmail.com>
2017-03-06 15:15:05 -0800

    Added score tracking

--- main.js
+++ main.js
@@ -46,7 +46,8 @@ while(gameflag==true){
 				alert("You move east. You are standing amidst the crumbling ruins of an ancient graveyard. A skeleton is roaming around the area holding a useful looking shield.")
 				var dirloc0 = prompt("Attenpt to (f)ight the skeleton for the shield, or return to the (w)est.")
 					if (dirloc0 == "f" && sword == true){
-						alert(" You use your sword to vanquish the skeleton and acquire the shield. There is nothing else to do in this area, so you return to the west.")
+						alert(" You use your sword to vanquish the skeleton and acquire the shield. [Score + 10] for slaying skeleton! [Score + 10] for shield get! There is nothing else to do in this area, so you return to the west.")
+						score += 20;
 						advloc = 1;
 						shield = true; 
 					}
@@ -65,7 +66,8 @@ while(gameflag==true){
 				alert("This is the meadow in which you found the sword. There is nothing else to do here so you head back north")
 				advloc = 1;}
 			else{
-				alert("You move to the south. You are standing in a large meadow. A broadsword is protruding from the corpse of a lizardman. Retrieiving the sword you begin to feel more confident that you will survive this adventure. Nothing else of interest is in this area and you return to the north.")
+				alert("You move to the south. You are standing in a large meadow. A broadsword is protruding from the corpse of a lizardman. Retrieiving the sword you begin to feel more confident that you will survive this adventure. [Score + 10] for sword get! Nothing else of interest is in this area and you return to the north.")
+				score += 10;
 				sword = true;
 				advloc = 1;
 			}
@@ -76,33 +78,40 @@ while(gameflag==true){
 				advloc = 1;
 			}
 			else {
-				alert("The shop here is in complete shambles. It appears to have once been an Apothecary. After a thorough search you manage to find a single intact health potion. Finding nothing else useful, you return west.")
+				alert("The shop here is in complete shambles. It appears to have once been an Apothecary. After a thorough search you manage to find a single intact health potion. [Score + 10] for potion get! Finding nothing else useful, you return west.")
 				advloc = 1;
 				potion = true;
+				score += 10;
 			}
 			break;
 		case 6:
 			alert(" You enter the cave. Before you lies a wicked looking red dragon lying upon a massive horde of gold and jewels. He eyes you with a hungry grin and lunges forward on the attack!")
 			if (sword == true && shield == true && potion == true){
-				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. With your left arm you quickly raise up your shield to block the dragon's flame breath while swinging your sword in a powerful arc to decapitate the beast. You quaff the healing potion lest you succumb to your burn and scratch wounds. You have defeated the dragon and lived. YOU WON THE GAME!")
+				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. With your left arm you quickly raise up your shield to block the dragon's flame breath while swinging your sword in a powerful arc to decapitate the beast. You quaff the healing potion lest you succumb to your burn and scratch wounds. You have defeated the dragon and lived. YOU WON THE GAME! [Score + 100] killed the dragon and lived!")
 				gameflag = false;
+				score += 100;
+				alert("Your score was " + score)
 			}
 			else if (sword == true && shield == true && potion == false){
-				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. With your left arm you quickly raise up your shield to block the dragon's flame breath while swinging your sword in a powerful arc to decapitate the beast. Alas you sustained too much damage during the fight and with no means to heal yourself, you succumb to your injuries. You defeated the dragon but you are dead. GAME OVER")
+				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. With your left arm you quickly raise up your shield to block the dragon's flame breath while swinging your sword in a powerful arc to decapitate the beast. Alas you sustained too much damage during the fight and with no means to heal yourself, you succumb to your injuries. You defeated the dragon but you are dead. [Score + 40] killed the dragon but died! GAME OVER")
 				advloc = 7;
+				score += 40;
 			}
 			else{
-				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. You spin around to see the dragon preparing a viscious flame breath attack and with no means to defend yourself from it you are killed. GAME OVER")
+				alert(" You roll away dodging the brunt of the dragons wicked claw attack but still take a significant amount of damage. You spin around to see the dragon preparing a viscious flame breath attack and with no means to defend yourself from it you are killed. [Score + 10] fed the hungry dragon! GAME OVER")
 				advloc = 7;
+				score += 10;
 			} 
 			break;
 		case 7:
+			alert("Your score was " + score)
 			alert("You had a bad end. Don't give up try again!")
 			gameflag = confirm (" Would you like to try again?")
 			advloc = 1;
 			var shield = false
 			var sword = false
 			var potion = false
+			score = 0
 			break;
 	}
 }
